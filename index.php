77039782c21597e70a96ff634d87b86dc658dce9
Jhostin <jjuarezl12@miumg.edu.gt>
2024-05-28 11:29:17 -0600

    modif

--- index.php
+++ index.php
@@ -35,8 +35,7 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 	    <meta name="robots" content="all">
 
 	    <title>Coffe shop UMG</title>
-	    <title>COFFE SHOP UMG TEST</title>
-	    <title>COFFE SHOP UMG</title>
+	    
 
 	    <!-- Bootstrap Core CSS -->
 	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
